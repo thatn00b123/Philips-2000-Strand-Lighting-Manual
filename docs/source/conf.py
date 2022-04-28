66dfbb45097d5e8b9b005c22fcb4e57a2015a59c
thatn00b123 <92887119+thatn00b123@users.noreply.github.com>
2022-04-28 08:30:53 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Philips-2000-Strand-Lighting-Manual'
+copyright = '2022, NRMS Theatre'
+author = 'Jacob Morrison'
 
 release = '0.1'
 version = '0.1.0'
